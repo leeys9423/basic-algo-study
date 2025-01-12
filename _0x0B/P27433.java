9a041c57d48b77e75524df61cc2e0e64d5decb39
leeys9423 <leeys9423@gmail.com>
2025-01-13 06:14:23 +0900

    재귀 - 팩토리얼2

---
+++ _0x0B/P27433.java
@@ -0,0 +1,20 @@
+package _0x0B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P27433 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(factorial(n));
+    }
+
+    static long factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+}
