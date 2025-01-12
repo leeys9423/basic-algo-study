eeba40cc620deaf1fac3f18ac0d8458a5f3c83dc
leeys9423 <leeys9423@gmail.com>
2025-01-13 06:19:29 +0900

    재귀 - 피보나치 수 5

---
+++ _0x0B/P11729.java
@@ -0,0 +1,14 @@
+package _0x0B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11729 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+
+    }
+}
